9a896299f1f9e0f596bb03feaeecccfa65390e22
PNatalya <nu_r@mail.ru>
2018-02-08 16:49:31 +0300

    Модуль 3 задание 1

--- templates/index.php
+++ templates/index.php
@@ -36,15 +36,15 @@
 			<?php foreach ($ads as $key=> $val): ?> 
 				<li class="lots__item lot">
 					<div class="lot__image">
-						<img src=<?=$val['img'];?> width="350" height="260" alt=<?=$val['name'];?>>
+						<img src=<?=$val['img'];?> width="350" height="260" alt=<?=htmlspecialchars($val['name']);?>>
 					</div>
 					<div class="lot__info">
 						<span class="lot__category"><?=$val['category'];?></span>
-						<h3 class="lot__title"><a class="text-link" href="lot.html"><?=$val['name'];?></a></h3>
+						<h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($val['name']);?></a></h3>
 						<div class="lot__state">
 							<div class="lot__rate">
 								<span class="lot__amount">Стартовая цена</span>
-								<span class="lot__cost"><?=format_sum($val['price']);?></span>
+								<span class="lot__cost"><?=format_sum(strip_tags($val['price']));?></span>
 							</div>
 							<div class="lot__timer timer">
 
