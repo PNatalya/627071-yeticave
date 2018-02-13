36b2cfae9db1f9b50b91fa48e5ced36150f7cef9
PNatalya <nu_r@mail.ru>
2018-02-13 14:22:28 +0300

    Модуль 4 задание 1

--- functions.php
+++ functions.php
@@ -1,5 +1,23 @@
 <?php
 
+function format_sum($price) {
+	$price = ceil($price);
+	If ($price >= 1000) {
+		$price = number_format ( $price , 0 , "." , " " );
+	};
+	$price .= "&nbsp;&#8381";
+
+	return $price;
+}
+
+function timelot($timelot) {
+	$timelot = $timelot - time();
+	$hour = floor($timelot/3600);
+	$minut = floor(($timelot%3600)/60);
+	$timelot = sprintf("%02d:%02d", $hour, $minut);	
+	
+	return $timelot;
+}
 
 function Include_Template($filename, $arr = array()) {
 	
